ece6573b4658d581e2c20d8d111973f755087306
Didan1771 <didan1771@icloud.com>
2024-05-26 16:50:30 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,23 @@
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3],
+          [5, 5, 5, 4, 5]]
+students = ['Johny', 'Bilbo', 'Steve', 'Khendrick', 'Aaron']
+print(students)
+
+
+print('Средний балл', students[4],'равен ', sum(grades[0]) / len(grades[0]))
+print('Средний балл', students[1],'равен ', sum(grades[1]) / len(grades[1]))
+print('Средний балл', students[0],'равен ', sum(grades[2]) / len(grades[2]))
+print('Средний балл', students[3],'равен ', sum(grades[3]) / len(grades[3]))
+print('Средний балл', students[2],'равен ', sum(grades[4]) / len(grades[4]))
+
+
+
+
+
+
+
+
+
+
+
